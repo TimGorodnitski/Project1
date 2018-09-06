535083c44448a3157f8529c3c88ccd1db06c4136
Tim Gorodnitski <timgm3@gmail.com>
2018-09-06 14:20:46 -0700

    small changes

--- logic.js
+++ logic.js
@@ -64,7 +64,7 @@ function initMap() {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
                 var place = results[i];
-                //  createMarker(results[i]);
+                createMarker(results[i]);
             }
         }
     }
