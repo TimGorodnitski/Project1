61d72c5551e73cf70f9f9b06069d1863691698cf
Tim Gorodnitski <timgm3@gmail.com>
2018-09-06 14:10:34 -0700

    master branch has working nav bar

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@ var map;
 var service;
 var infowindow;
 var geocoder;
-var address = "Alabama";
+var address = "UCLA";
 
 window.onload = function () {
 
@@ -37,38 +37,42 @@ function initMap() {
                     map: map,
                     position: results[0].geometry.location
                 });
+
+                marker.addListener('click', function () {
+                    infowindow.open(map, marker);
+                });
+
             } else {
                 alert('Geocode was not successful for the following reason: ' + status);
             }
-        })};
-
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
-
-        var request = {
-            query: '',
-            fields: ['photos', 'formatted_address', 'name', 'rating', 'opening_hours', 'geometry'],
-        };
-
-        service = new google.maps.places.PlacesService(map);
-        service.findPlaceFromQuery(request, callback);
+        })
+    };
 
 
 
-        function callback(results, status) {
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                    var place = results[i];
-                    //  createMarker(results[i]);
-                }
+    var request = {
+        query: '',
+        fields: ['photos', 'formatted_address', 'name', 'rating', 'opening_hours', 'geometry'],
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.findPlaceFromQuery(request, callback);
+
+
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                //  createMarker(results[i]);
             }
         }
-
-
-
-
     }
+
+
+
+
+}
 
 
 
