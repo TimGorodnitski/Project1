7405a816bae80db8f397dc31cd9efeca5a523c31
Tim Gorodnitski <timgm3@gmail.com>
2018-09-07 16:31:12 -0700

    maps kinda working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,7 @@
-var map;
 var service;
 var infowindow;
 var geocoder;
 var address = "UCLA";
-
 
 $.ajax({
     url: "http://beermapping.com/webservice/loccity/a75f6148bb9e67fc48410cb0be9bfe50/santa+monica,ca&s=json",
@@ -11,24 +9,16 @@ $.ajax({
 }).then(function (response) {
     console.log(response);
 
-
 });
 
 
-
-
 function initMap() {
-    var mapCenter = new google.maps.LatLng(34, -118.5);
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: mapCenter,
+    var mapCenter1 = new google.maps.LatLng(34, -118.5);
+    map = new google.maps.Map(document.getElementById('map1'), {
+        center: mapCenter1,
         zoom: 12
     });
 
-
-
-
-}
-
+};
 
 
