09482a35f50a61d0f7d6730ff11dd9f6d965cf8c
Tim Gorodnitski <timgm3@gmail.com>
2018-09-07 12:43:49 -0700

    still working on map div

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,17 +4,16 @@ var infowindow;
 var geocoder;
 var address = "UCLA";
 
-window.onload = function () {
 
-    $.ajax({
-        url: "http://beermapping.com/webservice/loccity/a75f6148bb9e67fc48410cb0be9bfe50/santa+monica,ca&s=json",
-        method: "get"
-    }).then(function (response) {
-        console.log(response);
+$.ajax({
+    url: "http://beermapping.com/webservice/loccity/a75f6148bb9e67fc48410cb0be9bfe50/santa+monica,ca&s=json",
+    method: "get"
+}).then(function (response) {
+    console.log(response);
 
 
-    });
-};
+});
+
 
 
 
@@ -25,49 +24,6 @@ function initMap() {
         center: mapCenter,
         zoom: 12
     });
-
-    geocoder = new google.maps.Geocoder();
-    codeAddress(geocoder, map);
-
-    function codeAddress(geocoder, map) {
-        geocoder.geocode({ 'address': address }, function (results, status) {
-            if (status === 'OK') {
-                map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: results[0].geometry.location
-                });
-
-                marker.addListener('click', function () {
-                    infowindow.open(map, marker);
-                });
-
-            } else {
-                alert('Geocode was not successful for the following reason: ' + status);
-            }
-        })
-    };
-
-
-
-    var request = {
-        query: '',
-        fields: ['photos', 'formatted_address', 'name', 'rating', 'opening_hours', 'geometry'],
-    };
-
-    service = new google.maps.places.PlacesService(map);
-    service.findPlaceFromQuery(request, callback);
-
-
-
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                var place = results[i];
-                createMarker(results[i]);
-            }
-        }
-    }
 
 
 
