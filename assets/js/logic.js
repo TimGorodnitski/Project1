750fd82ca66259261364a661e71a700d0490102d
TimG <timgm3@gmail.com>
2018-09-08 17:56:38 -0700

    all cities hardcoded, but only work once

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -40,10 +40,67 @@ function initMap() {
         center: mapCenter4,
         zoom: 12
     });
+
+    // Boston Map
+    var mapCenter5 = new google.maps.LatLng(42.3601, -71.0589);
+    map = new google.maps.Map(document.getElementById('map5'), {
+        center: mapCenter5,
+        zoom: 12
+    });
+
+    // Seattle Map
+    var mapCenter6 = new google.maps.LatLng(47.6062, -122.3321);
+    map = new google.maps.Map(document.getElementById('map6'), {
+        center: mapCenter6,
+        zoom: 12
+    });
+
+    // Kansas City Map
+    var mapCenter7 = new google.maps.LatLng(39.0997, -94.5786);
+    map = new google.maps.Map(document.getElementById('map7'), {
+        center: mapCenter7,
+        zoom: 12
+    });
+
+    // Raleigh Map
+    var mapCenter8 = new google.maps.LatLng(35.7796, -78.6382);
+    map = new google.maps.Map(document.getElementById('map8'), {
+        center: mapCenter8,
+        zoom: 12
+    });
+
+    // Milwaukee Map
+    var mapCenter9 = new google.maps.LatLng(43.0389, -87.9065);
+    map = new google.maps.Map(document.getElementById('map9'), {
+        center: mapCenter9,
+        zoom: 12
+    });
+
+    // New York Map
+    var mapCenter10 = new google.maps.LatLng(40.7128, -74.0060);
+    map = new google.maps.Map(document.getElementById('map10'), {
+        center: mapCenter10,
+        zoom: 12
+    });
+
+    // San Francisco Map
+    var mapCenter11 = new google.maps.LatLng(37.7749, -122.4194);
+    map = new google.maps.Map(document.getElementById('map11'), {
+        center: mapCenter11,
+        zoom: 12
+    });
+
+    // Chicago Map
+    var mapCenter12 = new google.maps.LatLng(41.8781, -87.6298);
+    map = new google.maps.Map(document.getElementById('map12'), {
+        center: mapCenter12,
+        zoom: 12
+    });
+
 };
 
 $(".thumb").on("click", function () {
-    $(this).find(".map").css("position", "initial").prependTo($(".poptrox-popup"));
+    $(this).find(".map").css("position", "initial").appendTo($(".poptrox-popup"));
     $(".poptrox-popup").css("overflow", "hidden");
 });
 
