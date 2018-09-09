5755099c8fafb8839bff01d1a69f3b496c278523
CaffeineKing <jacy_yahoo.com>
2018-09-08 19:04:59 -0700

    fixed arrow from pop up. fixed even pop up boxes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -118,6 +118,6 @@ function initMap() {
 $(".thumb").on("click", function () {
     $(this).find(".map").css("position", "initial").appendTo($(".poptrox-popup"));
     $(".poptrox-popup").css("overflow", "hidden");
+    
 });
-
 
